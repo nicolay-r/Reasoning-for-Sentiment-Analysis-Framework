3741e185a44e2eefde2d27cedbfa7e920fcbb403
Nicolay Rusnachenko <kolyarus@yandex.ru>
2024-03-16 12:32:58 +0000

    Switched link to the github raw content.

--- rusentne23_download.py
+++ rusentne23_download.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     data = {
         join(DATA_DIR, "train_data.csv"): "https://raw.githubusercontent.com/dialogue-evaluation/RuSentNE-evaluation/main/train_data.csv",
         join(DATA_DIR, "train_data_en.csv"): "https://www.dropbox.com/scl/fi/szj5j87f6w3ershnfh39x/train_data_en.csv?rlkey=h6ve617kl3o8g57otbt3yzamv&dl=1",
-        join(DATA_DIR, "valid_data.csv"): "https://www.dropbox.com/scl/fi/vthocgamkyxhqejyyw7hu/validation_data.csv?rlkey=dh3nk108vnfqrl8t3ituaq96o&dl=1",
+        join(DATA_DIR, "valid_data.csv"): "https://raw.githubusercontent.com/dialogue-evaluation/RuSentNE-evaluation/main/validation_data_labeled.csv",
         join(DATA_DIR, "valid_data_en.csv"): "https://www.dropbox.com/scl/fi/8cmocj7hqbbxpw8bop4vs/valid_data_en.csv?rlkey=6lqaz15lxs5x9hxemuhfv0fnk&dl=1",
         # Resources.
         join(DATA_DIR, "final_data.csv"): "https://raw.githubusercontent.com/dialogue-evaluation/RuSentNE-evaluation/main/final_data.csv",
