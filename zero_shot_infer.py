130dd7b1048603f0243bd2e133ccc076710e839f
Nicolay Rusnachenko <kolyarus@yandex.ru>
2024-05-31 17:29:22 +0100

    #9 Fix: removed non-used dependency

--- zero_shot_infer.py
+++ zero_shot_infer.py
@@ -9,6 +9,7 @@ from src.lm.gemma import Gemma
 from src.lm.microsoft_phi_2 import MicrosoftPhi2
 from src.lm.mistral import Mistral
 from src.lm.openai_chatgpt import OpenAIGPT
+from src.service_csv import CsvService
 from src.service_data import DataService
 from src.service_llm import chat_with_lm
 from src.service_sqlite import SQLiteProvider
